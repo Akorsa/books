cdaa9f3a2ef1cd3c0a0e99e405a53fd57b2c93a7
Artur Khalikov <a.khalikov@hh.ru>
2016-06-03 18:00:31 +0300

    java8 for impatient: collectors example

--- java8-for-impatient/src/main/java/chapter2/CollectorsExample.java
+++ java8-for-impatient/src/main/java/chapter2/CollectorsExample.java
@@ -5,34 +5,32 @@ import java.util.Properties;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class CollectorsExample {
+class CollectorsExample {
+
+  private static final Properties properties = new Properties();
 
   public static void main(String[] args) {
-
-    final Properties properties = new Properties();
     properties.put("com.application.setting1", "value1");
     properties.put("com.application.setting2", "value2");
     properties.put("com.test.testSetting", "testValue");
 
     System.out.println("prefix=com.application");
-    Map<String, String> subProperties1 = properties.stringPropertyNames().stream()
-        .filter(key -> key.startsWith("com.application"))
-        .collect(Collectors.toMap(Function.identity(), properties::getProperty));
-
-    subProperties1.forEach((key, value) -> System.out.println(key + ", " + value));
+    getSubProperties("com.application")
+        .forEach((key, value) -> System.out.println(key + ", " + value));
 
     System.out.println("\nprefix=com.test");
-    Map<String, String> subProperties2 = properties.stringPropertyNames().stream()
-        .filter(key -> key.startsWith("com.test"))
-        .collect(Collectors.toMap(Function.identity(), properties::getProperty));
-
-    subProperties2.forEach((key, value) -> System.out.println(key + ", " + value));
+    getSubProperties("com.test")
+        .forEach((key, value) -> System.out.println(key + ", " + value));
 
     System.out.println("\nprefix=com.missing");
-    Map<String, String> subProperties3 = properties.stringPropertyNames().stream()
-        .filter(key -> key.startsWith("com.missing"))
-        .collect(Collectors.toMap(Function.identity(), properties::getProperty));
+    getSubProperties("com.missing")
+      .forEach((key, value) -> System.out.println(key + ", " + value));
+  }
 
-    subProperties3.forEach((key, value) -> System.out.println(key + ", " + value));
+  static Map<String, String> getSubProperties(String prefix) {
+    return properties.stringPropertyNames()
+        .stream()
+        .filter(key -> key.startsWith(prefix))
+        .collect(Collectors.toMap(Function.identity(), properties::getProperty));
   }
 }
